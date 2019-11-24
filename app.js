cfd63889ca17f20a0f27d4508fd0ee49fede74f4
Marta Szymanska <info@martnet.pl>
2019-11-24 21:26:11 +0100

    Updates

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 window.addEventListener('load', (event) => {
-  
+
   var circle = document.getElementsByClassName('circle');
   for (i = 0; i < circle.length; i++) {
-    circle[i].addEventListener("click", clickCircle);
+    circle[i].addEventListener("click", loadChargingStatus);
   }
-  
-  function clickCircle() {
-    
-    
-    if(this.className.includes('circle-bg-green')) {
-      classReset();
-      this.className = 'circle';
-    } else {
-      classReset();
-      this.className = 'circle circle-bg-green';
-    }
-    LightsaberRand();
-  }
-  
+
   function classReset() {
     var circle = document.getElementsByClassName('circle');
     for (i = 0; i < circle.length; i++) {
       circle[i].className = 'circle';
     }
   }
-  
-  function LightsaberRand() {
-    var total_power = Math.floor((Math.random() * 7) + 1);
-    var LightsaberRand = Math.floor((Math.random() * total_power) + 1);
-    document.getElementById('total-power').innerHTML = total_power + 'kW';
-    
-    document.getElementById('Lightsaber').innerHTML = LightsaberRand+0 + 'kW';
-    document.getElementById('Falcon').innerHTML = total_power-LightsaberRand + 'kW';
+
+  function loadChargingStatus() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var status = JSON.parse(this.responseText);
+
+        var falcon = status.charging_status.falcon;
+        if (falcon > 0) {
+          document.getElementsByClassName('circle')[0].classList.add("circle-bg-green");
+        }
+
+        var lightsaber = status.charging_status.lightsaber;
+        if (lightsaber > 0) {
+          document.getElementsByClassName('circle')[1].classList.add("circle-bg-green");
+        }
+
+        document.getElementById('Falcon').innerHTML = falcon + 'kW';
+        document.getElementById('Lightsaber').innerHTML = lightsaber + 'kW';
+
+        var total_power = falcon + lightsaber;
+        document.getElementById('total-power').innerHTML = total_power + 'kW';
+     }
+    };
+    var type;
+    if (this.id == 'type-lightsaber'){
+      type = 'lightsaber';
+    } else if (this.id == 'type-falcon') {
+      type = 'falcon';
+    }
+    classReset();
+    this.classList.add("circle-bg-yellow");
+    xhttp.open("GET", "https://challenge.codetain.com/api/v1/charging_status?priority="+type, true);
+    xhttp.send();
   }
 });
-
